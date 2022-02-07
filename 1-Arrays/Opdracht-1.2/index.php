b04cff1691683327fa9df138e9a9f7e69094f162
Aydin Maleki <2148169@talnet.nl>
2022-02-07 13:09:10 +0100

    2

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$persoon["voornaam"]   = "Aydin";
+$persoon["achternaam"] = "Maleki";
+$persoon["leeftijd"]       = 18;
+$persoon["opleding"]   = "software developer";
+echo  $persoon['voornaam']. " " .  $persoon['achternaam'] . " is "  . $persoon['leeftijd'] . " jaar oud en is student aan het ROC van Amsterdam aan de opleiding  "  . $persoon['opleding'];
+?>
+
+</body>
+</html>
