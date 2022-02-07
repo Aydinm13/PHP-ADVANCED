4cf7cdb8bd02527fb378045c7a7cc4b8ab23c7a3
Aydin Maleki <2148169@talnet.nl>
2022-02-07 13:27:00 +0100

    2.1

---
+++ 2-Loops/Opdracht-2.1/loops-while.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$leeftijd = 0;
+
+while($leeftijd < 18){
+    
+    echo "Ik ben $leeftijd jaar, dus ik mag niet stemmen.";
+    echo"<br>";
+    $leeftijd++;
+}
+
+echo "Ik ben 18 jaar oud dus ik heb stemrecht!";
+?>
+
+</body>
+</html>
