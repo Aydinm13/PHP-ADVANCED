0e316bc430f7c71c64ca589c46b24e4a6c5e7191
Aydin Maleki <2148169@talnet.nl>
2022-02-07 12:55:43 +0100

    1

---
+++ 1-Arrays/Opdracht-1.1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$studenten = array("voornaam"=>"Aydin", "Achternaam"=>"Maleki", "Klas"=>"1C", "leeftijd"=>"18");
+echo $studenten["voornaam"];
+echo "<br>";
+echo $studenten["Achternaam"];
+echo "<br>";
+echo $studenten["Klas"];
+echo "<br>";
+echo $studenten["leeftijd"];
+?>
+
+</body>
+</html>
