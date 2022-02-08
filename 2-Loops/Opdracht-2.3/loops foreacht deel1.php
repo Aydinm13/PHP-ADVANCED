8ea87c11c0ae532e498eab7ae9a94595db787c3c
Aydin Maleki <2148169@talnet.nl>
2022-02-08 13:08:20 +0100

    fix

--- 2-Loops/Opdracht-2.3/loops foreacht deel1.php
+++ 2-Loops/Opdracht-2.3/loops foreacht deel1.php
@@ -3,13 +3,14 @@
 <body>
 
 <?php
+echo "<ul>";
 $dagen = array("maandag", "dinsadg", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag");
 
 foreach ($dagen as $value) {
     
-    echo "<ul><li>$value <br></li></ul>";
+    echo "<li>$value <br></li>";
 }
-
+echo "</ul>";
 ?>
 
 </body>
