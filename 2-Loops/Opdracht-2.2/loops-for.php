067156caf499d734e1ec9fe9d595666d1e42facd
Aydin Maleki <2148169@talnet.nl>
2022-02-08 13:05:51 +0100

    fix

--- 2-Loops/Opdracht-2.2/loops-for.php
+++ 2-Loops/Opdracht-2.2/loops-for.php
@@ -3,11 +3,13 @@
 <body>
 
 <?php
+echo "<ul>";
 for ($x = 0; $x < 18; $x++){
-    echo "<ul><li>Ik ben $x jaar, dus ik mag niet stemmen.</ul></li>";
+    echo "<li>Ik ben $x jaar, dus ik mag niet stemmen.</li>";
     echo"<br>";
 }
-echo "<ul><li>Ik ben 18 jaar oud dus ik heb stemrecht!</ul></li>";
+echo "<li>Ik ben 18 jaar oud dus ik heb stemrecht!</li>";
+echo "</ul>";
 ?>
 
 </body>
