9278600cd1ff8928ca9e56a51d567911c3bf9404
Aydin Maleki <2148169@talnet.nl>
2022-02-08 12:39:46 +0100

    3.2

---
+++ 3-Forms/Opdracht-3.2/mijngegevens.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+
+<?php 
+
+echo"voornaam: ".$_POST["voornaam"];
+echo"<br>";
+echo"achternaam: ".$_POST["achternaam"];
+echo"<br>";
+echo"klas: ".$_POST["klas"];
+echo"<br>";
+echo"leeftijd: ".$_POST["leeftijd"];
+echo"<br>";
+echo"adres: ".$_POST["adres"];
+echo"<br>";
+echo"plaatsnaam: ".$_POST["plaatsnaam"];
+echo"<br>";
+echo"favoriete muziekband: ".$_POST["favorietemuziekband"];
+echo"<br>";
+echo"geslacht is: ".$_POST["geslacht"];
+echo"<br>";
+echo $_POST["color"];
+?>
+<style>
+    body {
+        background-color: <?php echo $_POST["color"];?>;
+    }
+</style>
+</body>
+</html>
